c0988dcb5944454dfa61010ed099f13e389d2a36
kp1129 <katya.p.1129@gmail.com>
2020-01-14 18:15:03 -0700

    update index.js, meet mvp

--- js/index.js
+++ js/index.js
@@ -16,35 +16,76 @@ document.addEventListener('keydown', (event) => {
     alert('For help, please contact us using the form on the Contact page');
 });
 
-// wheel - scale that section
-//refactor so that this only happens on the whole section, and goes back to normal on mouseleave
+//wheel - change background color
+const colors = ['#ffffff', '#2e4045', '#83adb5', '#c7bbc9', '#5e3c58', '#bfb5b2', '#66545e', '#a39193', '#aa6f73', '#eea990', '#f6e0b5'];
+let ix = 0;
+const body = document.querySelector('body');
+body.addEventListener('wheel', () => {
+    let random = Math.floor(Math.random() * colors.length - 1);
+    body.style.backgroundColor = colors[random];
+});
 
-// const sections = document.querySelectorAll('section');
+// click - scale that section a bit
+const sections = document.querySelectorAll('section');
 
-// sections.forEach(section => {
-//     let scale = 1;
-//     section.addEventListener('wheel', (event) => {
-//         scale += event.deltaY * -0.01;
-//         scale = Math.min(Math.max(.125, scale), 4);
-//         event.target.style.transform = `scale(${scale})`;
-//     });
-// });
+sections.forEach(section => {
+    section.addEventListener('click', (event) => {
+        event.target.style.transform = `scale(1.2)`;
+    });
+});
+
+
 
 // dblclick on image - rotate
 const images = document.querySelectorAll('img');
 images.forEach(img => {
+    let clicked = false;
     img.addEventListener('dblclick', (event) => {
-        event.target.style.transform = 'rotate(5deg)';
+        event.target.style.transition = 'transform 0.5s';
+        if(!clicked) {        
+            event.target.style.transform = 'rotate(5deg)';  
+            clicked = true; 
+        } else {
+            event.target.style.transform = 'rotate(0deg)';
+            clicked = false;
+        }     
     });
 });
 
 //dragend - alert
-document.addEventListener('dragend', (event) => {
+document.addEventListener('dragend', () => {
     alert('This page has no drag and drop elements.');
 });
 
 // load - welcome
-window.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
     alert('Welcome!');
 });
 
+// resize - hide header image
+window.addEventListener('resize', () => {
+    document.querySelector('.intro img').style.visibility = 'hidden';
+});
+
+//mousedown - change menu item styles
+const navItems = document.querySelectorAll('.nav a');
+navItems.forEach(item => {
+    item.addEventListener('mousedown', (event) => {  
+        event.stopPropagation();      
+        event.target.style.color = '#00ff00';
+        event.target.style.fontWeight = 'bold';
+    });
+});
+
+// second mousedown to see event propagation irl
+header.addEventListener('mousedown', (event) => {
+    header.style.backgroundColor = 'black';
+    header.style.color = 'red';
+});
+
+// preventDefault()
+navItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();        
+    })
+});
